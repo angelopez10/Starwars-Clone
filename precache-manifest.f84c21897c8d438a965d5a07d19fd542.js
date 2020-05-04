e43b25fa3091a4a37fe84d5c64c398e20d84c752
angelopez10 <angelopez10898@gmail.com>
2020-05-04 10:20:07 -0400

    Updates

---
+++ precache-manifest.f84c21897c8d438a965d5a07d19fd542.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bcff983b93942d1057a536791174912",
+    "url": "//angelopez10.github.io/Starwars-Clone/index.html"
+  },
+  {
+    "revision": "c641c0e8aa09ef94497b",
+    "url": "//angelopez10.github.io/Starwars-Clone/static/css/2.9bfd1259.chunk.css"
+  },
+  {
+    "revision": "1444882b4d165622234f",
+    "url": "//angelopez10.github.io/Starwars-Clone/static/css/main.94db5503.chunk.css"
+  },
+  {
+    "revision": "c641c0e8aa09ef94497b",
+    "url": "//angelopez10.github.io/Starwars-Clone/static/js/2.ac082ed1.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "//angelopez10.github.io/Starwars-Clone/static/js/2.ac082ed1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1444882b4d165622234f",
+    "url": "//angelopez10.github.io/Starwars-Clone/static/js/main.23631246.chunk.js"
+  },
+  {
+    "revision": "e588ccc64ce21b6fbbf6",
+    "url": "//angelopez10.github.io/Starwars-Clone/static/js/runtime-main.ed2f0c23.js"
+  },
+  {
+    "revision": "067595ad77ecc0db9c81c8905a7eef32",
+    "url": "//angelopez10.github.io/Starwars-Clone/static/media/fa-brands-400.067595ad.woff2"
+  },
+  {
+    "revision": "57dcda6f368ea90179f75cbdae96c263",
+    "url": "//angelopez10.github.io/Starwars-Clone/static/media/fa-brands-400.57dcda6f.eot"
+  },
+  {
+    "revision": "9d67fa1429375bd2a899a17eb77d0342",
+    "url": "//angelopez10.github.io/Starwars-Clone/static/media/fa-brands-400.9d67fa14.svg"
+  },
+  {
+    "revision": "9ec698d1a597bff5df337094b71ddaaf",
+    "url": "//angelopez10.github.io/Starwars-Clone/static/media/fa-brands-400.9ec698d1.ttf"
+  },
+  {
+    "revision": "b564da88bbf0c4aa446fa19653713cd1",
+    "url": "//angelopez10.github.io/Starwars-Clone/static/media/fa-brands-400.b564da88.woff"
+  },
+  {
+    "revision": "3351f435b3c9037fd88aeb04dc1e43bc",
+    "url": "//angelopez10.github.io/Starwars-Clone/static/media/fa-regular-400.3351f435.eot"
+  },
+  {
+    "revision": "4165c2688309cbfb1b877caf8f75afb5",
+    "url": "//angelopez10.github.io/Starwars-Clone/static/media/fa-regular-400.4165c268.woff2"
+  },
+  {
+    "revision": "5d0861781aeef6c82fda3a3076954a1b",
+    "url": "//angelopez10.github.io/Starwars-Clone/static/media/fa-regular-400.5d086178.svg"
+  },
+  {
+    "revision": "73cf49a2232c06c920b7a34e36bfb58c",
+    "url": "//angelopez10.github.io/Starwars-Clone/static/media/fa-regular-400.73cf49a2.woff"
+  },
+  {
+    "revision": "a0e3ac82940c1998c5977fd4bc1f5ef6",
+    "url": "//angelopez10.github.io/Starwars-Clone/static/media/fa-regular-400.a0e3ac82.ttf"
+  },
+  {
+    "revision": "0724bb8b89ab6b8b9b7df917b17be0b7",
+    "url": "//angelopez10.github.io/Starwars-Clone/static/media/fa-solid-900.0724bb8b.svg"
+  },
+  {
+    "revision": "55eb2a60e8181f0e68b558c991973bf0",
+    "url": "//angelopez10.github.io/Starwars-Clone/static/media/fa-solid-900.55eb2a60.woff2"
+  },
+  {
+    "revision": "75f38a159982b6bd1704891332d95fa7",
+    "url": "//angelopez10.github.io/Starwars-Clone/static/media/fa-solid-900.75f38a15.ttf"
+  },
+  {
+    "revision": "89e02bae13c9131c7468b1e729339ac1",
+    "url": "//angelopez10.github.io/Starwars-Clone/static/media/fa-solid-900.89e02bae.eot"
+  },
+  {
+    "revision": "cdfec5cf5e9840889790bcf2c4042583",
+    "url": "//angelopez10.github.io/Starwars-Clone/static/media/fa-solid-900.cdfec5cf.woff"
+  },
+  {
+    "revision": "f425cfea9a892487470f45d3345d0032",
+    "url": "//angelopez10.github.io/Starwars-Clone/static/media/main-bg.f425cfea.jpeg"
+  }
+]);
